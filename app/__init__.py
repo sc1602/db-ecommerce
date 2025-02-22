256d5368f9a2d005e67095a18bc45647b47c837e
sc1602 <walter1602br@gmail.com>
2025-02-21 20:02:19 -0500

    se agrego flask-cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from db import db
 from flask_migrate import Migrate
 from config import DevelopmentConfig
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
 migrate = Migrate(app, db)
